1af5dfcb1f6f36d906a6fdbe0862033f4e06f761
davidlin <aunbdaunbd@gmail.com>
2019-03-09 11:54:38 +0800

    Task4 (add models) finished. There is something wrong with task3. The homework's requirement is to rotate where you navigate from. But I rotate the models itselves. I'll fix it later.

--- HW01/HW1_starter/main.cpp
+++ HW01/HW1_starter/main.cpp
@@ -59,6 +59,7 @@ bool handleEvents(SDL_Event & evt, sdlWrapper & sdlContext);
 // You do not have to use these if you don't want
 
 #include <math.h>
+#define	MODEL_NUM	3
 
 // parameters for the navigation
 glm::vec3	viewerPosition	(0.0, 0.0, 50.0);
@@ -75,8 +76,57 @@ float	lastXOffset = 0.0, lastYOffset = 0.0, lastZOffset = 0.0;
 enum MouseState { mouseIdle, leftMouseButtonActive, middleMouseButtonActive, rightMouseButtonActive };
 MouseState mouseState = mouseIdle;
 
-// Position of the objects.
-glm::vec3	teapotPos(0.0f, 0.0f, 0.0f);
+// Data structure for the model.
+class ModelObjects {
+	/* Data members. */
+	Model *model;
+	glm::vec3 pos;
+	glm::vec3 scale;
+	glm::mat4 modelMat;
+
+public:
+	/* Constructor. */
+	ModelObjects() {
+		model = NULL;
+		pos = glm::vec3(0.0f), scale = glm::vec3(0.0f);
+		modelMat = glm::scale(glm::translate(glm::mat4(1.0f), pos), scale);
+	}
+	ModelObjects(string const& path, glm::vec3 const& posIn, glm::vec3 const& scaleIn) {
+		initModel(path, pos, scale);
+	}
+	/* Deconstructor. */
+	~ModelObjects() {
+		delete model;
+		model = NULL;
+	}
+
+	/* Member functions. */
+	void initModel(string const& path, glm::vec3 const& posIn, glm::vec3 const& scaleIn) {
+		model = new Model(modelsDir + path);
+		pos = posIn, scale = scaleIn;
+		modelMat = glm::scale(glm::translate(glm::mat4(1.0f), pos), scale);
+	}
+
+	// Functions for model.
+	Model getModel() { return *model; }
+
+	// Functions for position.
+	void setPos(glm::vec3 const& posIn) { pos = posIn; }
+	glm::vec3 const& getPos() { return pos; }
+
+	// Functions for scale.
+	void setScale(glm::vec3 const& scaleIn) { scale = scaleIn; }
+	glm::vec3 const& getScale() { return scale; }
+
+	// Functions for modelMat.
+	void setModelMat() {
+		glm::mat4 t  = glm::translate(glm::mat4(1.0f), pos);
+		glm::mat4 rx = glm::rotate( t, glm::radians(navigationRotation[0]), glm::vec3(1.0f, 0.0f, 0.0f));
+		glm::mat4 ry = glm::rotate(rx, glm::radians(navigationRotation[1]), glm::vec3(0.0f, 1.0f, 0.0f));
+		modelMat = glm::scale(ry, scale);
+	}
+	glm::mat4 const& getModelMat() { return modelMat; }
+} models[MODEL_NUM];
 
 void setMousePressedPos(Sint32 const& x, Sint32 const& y) {
 	mousePressedX = x, mousePressedY = y;
@@ -120,7 +170,9 @@ void main(int argc, char **argv) {
 	// ************************* Load Your Models Here ****************************
 	// ****************************************************************************
 	std::cout << "Loading Models ...";
-	Model model(modelsDir + "teapot.obj");
+	models[0].initModel(modelsDir + "teapot.obj", glm::vec3(0.0f), glm::vec3(10.0f));
+	models[1].initModel(modelsDir + "teapot.obj", glm::vec3(20.0f, 0.0f, 0.0f), glm::vec3(10.0f));
+	models[2].initModel(modelsDir + "teapot.obj", glm::vec3(-20.0f, 0.0f, 0.0f), glm::vec3(10.0f));
 	Model axes(modelsDir + "axes.obj");
 	std::cout << " Done!" << std::endl;
 	// ****************************************************************************
@@ -133,51 +185,44 @@ void main(int argc, char **argv) {
 	while (running) {
 
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		
+
 		running = handleEvents(evt, sdlContext);
 
 		glViewport(0, 0, sdlContext.getWidth(), sdlContext.getHeight());
 
 		// Always call this before you attach something to uniform, or initiate any draw calls
 		shader.Use();
-		
+
 		// ****************************************************************************
 		// ************************** Insert your Code here ***************************
 		// ****************************************************************************
 
 		// Setup Projection, Model, and View Matricies
 		glm::mat4 projMat = glm::perspective(glm::radians(50.0f), aspectRatio, zNear, zFar);
-		glm::mat4 modelMat = glm::scale(
-			glm::rotate(	// x axes
-				glm::rotate(	// y axes
-					glm::translate(glm::mat4(1.0f), teapotPos),
-					glm::radians(navigationRotation[0]),
-					glm::vec3(1.0f, 0.0f, 0.0f)
-				),
-				glm::radians(navigationRotation[1]),
-				glm::vec3(0.0f, 1.0f, 0.0f)
-			), 
-			glm::vec3(10.0f)
-		);
 		glm::mat4 viewMat = glm::lookAt(viewerPosition, viewerCenter, viewerUp);
+		for (int i = 0; i < MODEL_NUM; i++)
+			models[i].setModelMat();
 
 		// Attached Projection, Model, and View matricies to the shader
 		// In the shader the Proj * View * Model * vertex_coord operation is carried out
 		shader.attachToUniform("Proj", projMat);
 		shader.attachToUniform("View", viewMat);
-		shader.attachToUniform("Model", modelMat);
+		shader.attachToUniform("Model", models[0].getModelMat());
 
 		axes.Draw(shader);
-		model.Draw(shader);
+		models[0].getModel().Draw(shader);
+		for (int i = 1; i < MODEL_NUM; i++) {
+			shader.attachToUniform("Model", models[i].getModelMat());
+			models[i].getModel().Draw(shader);
+		}
 
 		// ****************************************************************************
 		// ****************************************************************************
 		// ****************************************************************************
-		
+
 		sdlContext.swapbuffer();
 	}
 
-		
 }
 
 //**************************************************************
@@ -272,7 +317,8 @@ bool handleEvents(SDL_Event & evt, sdlWrapper & sdlContext) {
 				tmpXOff = (float)(evt.motion.x - mousePressedX);
 				tmpYOff = (float)(mousePressedY - evt.motion.y);
 				// Update object's position.
-				teapotPos += glm::vec3(tmpXOff - lastXOffset, tmpYOff - lastYOffset, 0.0f);
+				for (int i = 0; i < MODEL_NUM; i++)
+					models[i].setPos(models[i].getPos() + glm::vec3(tmpXOff - lastXOffset, tmpYOff - lastYOffset, 0.0f));
 				setMouseLastOffset(tmpXOff, tmpYOff, 0.0f);
 				break;
 
@@ -280,7 +326,8 @@ bool handleEvents(SDL_Event & evt, sdlWrapper & sdlContext) {
 				// Calculate the offset of X.
 				tmpXOff = (float)(mousePressedX - evt.motion.x);
 				// Update object's position.
-				teapotPos += glm::vec3(0.0f, 0.0f, tmpXOff - lastXOffset);
+				for (int i = 0; i < MODEL_NUM; i++)
+					models[i].setPos(models[i].getPos() + glm::vec3(0.0f, 0.0f, tmpXOff - lastXOffset));
 				setMouseLastOffset(tmpXOff, tmpYOff, 0.0f);
 				break;
 
