028d4ede7b2eeb75691fd370189f6adce42b3101
davidlin <aunbdaunbd@gmail.com>
2019-04-13 16:18:48 +0800

    Add Teeth.cs.
    Description:
        It contains the teeth datas. Only 1 instance will use in
        the application ( except in importing final teeth, there
        will be a temporary Teeth instance).
    Details:
        1. obj: GameObject of teeth.
        2. param: Parameters of each tooth. Including v1, v2 and
           v3, lingual position and center.
        3. TOOTH_NUM: The numbers of the teeth.

---
+++ Final/Scripts/Teeth.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teeth : MonoBehaviour
+{
+    public GameObject[] obj;
+    public Tooth.ToothParam[] param;
+    public readonly uint TOOTH_NUM = 32;
+}
